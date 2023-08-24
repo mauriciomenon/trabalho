6e92c4b30b5677c60b16fdfaf7830943db1f2eb7
menon <MENON@ITAIPU.GOV.BR>
2023-08-24 17:13:55 -0300

    +

--- sendTrapIEE1.py
+++ sendTrapIEE1.py
@@ -1,30 +1,41 @@
-import snmpy
+from pysnmp.hlapi import *
+from pysnmp.smi import view
+
+# Função para converter string OID para tupla
+
+
+def oid_str_to_tuple(oid_str):
+    return tuple(map(int, oid_str.split('.')))
 
 # Função para verificar se o OID está registrado
 
 
-def is_oid_registered(mib_controller, oid_str):
+def is_oid_registered(mib_view, oid_str):
+    oid_tuple = oid_str_to_tuple(oid_str)
     try:
-        result = mib_controller.get(oid_str)
+        result = mib_view.getNodeName(oid_tuple)
         return result is not None
-    except snmpy.NoSuchObjectError:
+    except Exception:
         return False
 
 
 # Criação de um engine SNMP
-snmp_engine = snmpy.SnmpEngine()
+snmp_engine = SnmpEngine()
 
-# Configuração do receptor de trap
-trap_receiver = snmpy.TrapReceiver(snmp_engine, ('0.0.0.0', 162))
+# Criação do MibViewController
+mib_view = view.MibViewController(snmp_engine.getMibBuilder())
+
+# Configuração do receptor de trap (Estou ignorando essa parte porque não estamos configurando traps aqui)
+# trap_receiver = TrapReceiver(snmp_engine, ('0.0.0.0', 162))
 
 # Definindo OID e seu valor
 oid_str = '1.3.6.1.2.1.1.1.0'
 
 # Verificação se o OID já está registrado
-if not is_oid_registered(snmp_engine.mib_controller, oid_str):
-    snmp_engine.mib_builder.import_symbols('SNMPv2-MIB', 'sysDescr')
-    snmp_engine.mib_builder.define_scalar(
-        oid_str, snmpy.Integer32(), max_access="readwrite")
+if not is_oid_registered(mib_view, oid_str):
+    snmp_engine.getMibBuilder().importSymbols('SNMPv2-MIB', 'sysDescr')
+    snmp_engine.getMibBuilder().defineScalar(
+        oid_str_to_tuple(oid_str), Integer32(), maxAccess="readwrite")
 
-# Roda o servidor indefinidamente para receber traps
-trap_receiver.run_forever()
+# Roda o servidor indefinidamente para receber traps (ignorado pois não foi mostrado como fazer isso com pysnmp)
+# trap_receiver.run_forever()
